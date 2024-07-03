61e973b22179d1bdec8ef966771ba223c6888bea
judz5 <judz1105@gmail.com>
2024-07-03 16:55:35 -0500

    stop using curses

--- Trace.py
+++ Trace.py
@@ -1,12 +1,11 @@
 import socket
 import requests
-import curses
 import folium
 from folium.plugins import AntPath
 
 port = 33434
 
-logo = """
+logo = r"""
   ______ _______  _____  _______  ______ _______ _______ _______
  |  ____ |______ |     |    |    |_____/ |_____| |       |______
  |_____| |______ |_____|    |    |    \_ |     | |_____  |______
@@ -14,54 +13,24 @@ logo = """
 """
 
 
-def print_logo(stdscr):
-    global logo_line
+def print_logo():
+    print(logo)
 
-    height, width = stdscr.getmaxyx()
-
-    logo_line = logo.splitlines()
-    for i, line in enumerate(logo_line):
-        stdscr.addstr(2 + i, (width // 2) - (len(line) // 2), line)
-
-    prompt = "Enter URL: "
-    stdscr.addstr(4 + len(logo_line), (width // 2) -
-                  (len(prompt) // 2), prompt)
-
-
-def get_user_url(stdscr):
-    curses.curs_set(1)
-    stdscr.clear()
-
-    height, width = stdscr.getmaxyx()
-
-    print_logo(stdscr)
-
-    prompt = "Enter URL: "
-    stdscr.addstr(4 + len(logo_line), (width // 2) -
-                  (len(prompt) // 2), prompt)
-
-    curses.echo()
-    stdscr.refresh()
-
-    url = stdscr.getstr(5 + len(logo_line), (width // 2) - 10).decode('utf-8')
-    curses.noecho()
+def get_user_url():
+    print_logo()
+    url = input("Enter URL/IP: ")
     return url
 
+def main():
 
-def main(stdscr):
+    site = get_user_url()
 
-    curses.curs_set(1)
 
-    site = get_user_url(stdscr)
-
-    stdscr.clear()
-
-    print_logo(stdscr)
+    print_logo()
 
     # Get target IP addr
     dest_ip = socket.gethostbyname(site)
-    stdscr.addstr(2 + len(logo_line), 0, f'Tracing path to : {dest_ip}')
-    stdscr.refresh()
+    print(f'Tracing path to : {dest_ip}')
 
     # Creates a UDP Socket for sending out packets
     sent_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -117,33 +86,29 @@ def main(stdscr):
             cords.append((loc_data.get("lat"), loc_data.get("lon"), num_hop))
             info = ('Hop #' + str(num_hop) + ": " + hopped_ip + ", " + str(loc_data.get("city")) +
                     ", " + str(loc_data.get("region")) + ", " + str(loc_data.get("country")))
-            stdscr.addstr((2 + len(logo_line)) + num_hop, 0, info)
-            stdscr.refresh()
+            print(info)
 
         except socket.timeout:
-            stdscr.addstr((2 + len(logo_line)) + num_hop, 0,
-                          f'Hop #{num_hop}: Request timed out')
-            stdscr.refresh()
+            print(f'Hop #{num_hop}: Request timed out')
 
         if hopped_ip == dest_ip:
             break
 
         num_hop += 1
 
+    pingMap = folium.Map(zoom_start=5)
+    for lat, lon, hop in cords:
+        if lat is not None and lon is not None:
+            folium.Marker(location=(lat, lon), popup=f'Hop #{hop}').add_to(pingMap)
 
-curses.wrapper(main)
+    # ant path for showing order
+    lat_lon_list = [(lat, lon) for lat, lon,
+                    _ in cords if lat is not None and lon is not None]
+    if lat_lon_list:
+        AntPath(lat_lon_list).add_to(pingMap)
 
-pingMap = folium.Map(zoom_start=5)
-for lat, lon, hop in cords:
-    if lat is not None and lon is not None:
-        folium.Marker(location=(lat, lon), popup=f'Hop #{hop}').add_to(pingMap)
+    pingMap.save("traceMap.html")
 
-# ant path for showing order
-lat_lon_list = [(lat, lon) for lat, lon,
-                _ in cords if lat is not None and lon is not None]
-if lat_lon_list:
-    AntPath(lat_lon_list).add_to(pingMap)
+    print(f'dest found in {num_hop} hops, at location {cords[-1]}')
 
-pingMap.save("traceMap.html")
-
-print(f'dest found in {num_hop} hops, at location {cords[-1]}')
+main()
